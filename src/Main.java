c0a78641a0822aeb7f2cb0db5652d609c58e022a
Denis Panasyuk <den.dzim@yandex.ru>
2025-07-09 22:39:23 +0300

    переписал тесты

--- src/Main.java
+++ src/Main.java
@@ -17,7 +17,7 @@ public class Main {
 
         System.out.println(taskManager.getTaskForId(id)); //вывод после замены таска
         System.out.println(taskManager.getTaskList());
-        System.out.println();
+        System.out.println("Тестирование эпиков");
         Epic epic1 = new Epic("Эпик1", "Описание 1",TaskStatus.NEW);
         taskManager.addEpic(epic1);
         Subtask subtask1 = new Subtask("подзадача 1", "тело", epic1.getId(), TaskStatus.NEW);
@@ -30,28 +30,46 @@ public class Main {
         taskManager.addSubtask(subtask1);
         taskManager.addSubtask(subtask2);
         taskManager.addSubtask(subtask3);
-        System.out.println();
+        System.out.println("печатаем список тасков");
         System.out.println(taskManager.getTaskList());
-        System.out.println("****************");
+        System.out.println("печатаем список эпиков");
         System.out.println(taskManager.getEpicList());
-        System.out.println("****************");
+        System.out.println("печатаем все подзадачи");
         System.out.println(taskManager.getAllSubtask());
-        System.out.println("****************");
+        System.out.println("печатаем субтаски 2 эпика");
+        System.out.println(taskManager.getEpicSubtask(4));
+        System.out.println("выводим статус подзадачи 2 эпика 2");
         System.out.println(subtask2.getStatus());
+        System.out.println("меняем статус этой подзадачи");
         subtask2.setStatus(TaskStatus.IN_PROGRESS);
+        taskManager.updateSubtask(subtask2);
+        System.out.println("выводим статус подзадачи 2 эпика 2");
         System.out.println(subtask2.getStatus());
+        System.out.println("выводим эпик по ай ди ");
         System.out.println(taskManager.getEpicForId(4)); //проверка смены статуса
-        System.out.println();
+        System.out.println("меняем статус 3 подзадачи эпика 2");
         subtask3.setStatus(TaskStatus.DONE);
+        taskManager.updateSubtask(subtask3);
+        System.out.println(subtask3.getStatus());
+        System.out.println("выводим этот же эпик");
         System.out.println(taskManager.getEpicForId(4));
+        System.out.println("меняем статус 2 подзадачи эпика 2");
         subtask2.setStatus(TaskStatus.DONE);
+        taskManager.updateSubtask(subtask2);
+        System.out.println(subtask2.getStatus());
+        System.out.println("выводим этот же эпик");
         System.out.println(taskManager.getEpicForId(4));
-        System.out.println();
+        System.out.println("меняем статус 3 подзадачи эпика 2");
         subtask3.setStatus(TaskStatus.IN_PROGRESS);
-        System.out.println(subtask2);
+        taskManager.updateSubtask(subtask3);
+        System.out.println(subtask3.getStatus());
+        System.out.println("выводим этот же эпик");
+        System.out.println(taskManager.getEpicForId(4));
+        System.out.println("удаляем подзадачу 2 эпика 2");
         taskManager.removeSubtaskForId(6);
-        System.out.println();
-        System.out.println(taskManager.getEpicForId(4)); //проверка статуса эпика после удаления подзадачи
+        System.out.println(taskManager.getEpicSubtask(4));
+        System.out.println("выводим этот же эпик");
+        System.out.println(taskManager.getEpicForId(4));//проверка статуса эпика после удаления подзадачи
         taskManager.deleteTaskList();
         taskManager.deleteAllSubtask();
         taskManager.deleteAllEpic();
