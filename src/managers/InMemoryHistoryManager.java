a4687b46d6e3b4523417756fa558ef1b89f99ffa
Denis Panasyuk <den.dzim@yandex.ru>
2025-08-12 22:55:50 +0300

    Написана своя реализация связанного списка

--- src/managers/InMemoryHistoryManager.java
+++ src/managers/InMemoryHistoryManager.java
@@ -10,47 +10,64 @@ import java.util.List;
 
 public class InMemoryHistoryManager implements HistoryManager {
 
-    private HashMap<Integer, Node> testHistory = new HashMap<>();
-
-    private Node<Task> head;
-    private Node<Task> tail;
-    private int size = 0;
+    private HashMap<Integer, Node> mapHistory = new HashMap<>();
+    private Node head;
+    private Node tail;
 
     @Override
     public void add(Task task) {
         if (task == null) {
             return;
         }
+        final int id = task.getId();
+        removeNode(id);
         linkLast(task);
-        if (testHistory.containsKey(task.getId())) {
+        mapHistory.put(id, head);
+    }
 
+    private ArrayList<Task> getTasks() {
+        ArrayList<Task> historyList = new ArrayList<>();
+        for (Integer id : mapHistory.keySet()) {
+            historyList.add(mapHistory.get(id).task);
         }
-
+        return historyList;
     }
 
     @Override
     public List<Task> getHistory() {
-        ArrayList<Task> historyList = new ArrayList<>(testHistory)
-        return copyHistory;
+        return getTasks();
     }
 
-    @Override
-    public void remove(int id) {
-        testHistory.remove(id);
-    }
-
-    public void linkLast(Task task) {
-        final Node<Task> oldTail = tail;
-        final Node<Task> newNode = new Node<>(oldTail, task, null);
+    private  void linkLast(Task task) {
+        final Node oldTail = tail;
+        final Node newNode = new Node(oldTail, task, null);
         tail = newNode;
         if (oldTail == null)
             head = newNode;
         else
             oldTail.next = newNode;
-        size++;
     }
 
-    public void removeNode(Node node) {
+    private void removeNode(int id) {
+        Node node = mapHistory.remove(id);
+        if (tail == head) {
+            tail.prev = null;
+            head.next = null;
+        }
+        if (node == tail) {
+            tail = node.next;
+            tail.prev = null;
+        } else if (node == head) {
+              head = node.prev;
+              head.next = null;
+        } else {
+            node.prev.next = node.next;
+            node.next.prev = node.prev;
+        }
+    }
 
+    @Override
+    public void remove(int id) {
+        removeNode(id);
     }
 }
--- src/tasks/Node.java
+++ src/tasks/Node.java
@@ -1,13 +1,22 @@
 package tasks;
 
-public class Node <Task> {
+public class Node {
     public Task task;
-    public Node<Task> next;
-    public Node<Task> prev;
+    public Node next;
+    public Node prev;
 
-    public Node(Node<Task> prev, Task task, Node<Task> next) {
+    public Node(Node prev, Task task, Node next) {
         this.task = task;
         this.next = null;
         this.prev = null;
+    }
+
+    @Override
+    public String toString() {
+        return "Node{" +
+                "task=" + task +
+                ", next=" + next + (next == null ? null : next.task) +
+                ", prev=" + prev + (prev == null ? null : prev.task) +
+                '}';
     }
 }
